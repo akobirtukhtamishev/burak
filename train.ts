cbaf98dbe6b44568ddcfa52fa7ef4577c2ed7b6c
akobirtukhtamishev <akobirtukhtamishev@gmail.com>
2026-09-23 20:55:06 +0900

    task M is completed

--- train.ts
+++ train.ts
@@ -1,12 +1,30 @@
-function reverseSentence(str: string): string {
-    let words: string[] = str.split(" ");
-    let result: string[] = [];
+function getSquareNumbers(arr: number[]) {
+    let result = [];
 
-    for (let i = 0; i < words.length; i++) {
-        result.push(words[i].split("").reverse().join(""));
+    for (let i = 0; i < arr.length; i++) {
+        result.push({
+            number: arr[i],
+            kvadrat: arr[i] * arr[i]
+        });
     }
 
-    return result.join(" ");
+    return result;
 }
 
-console.log(reverseSentence("my name is ethan"));
+console.log(getSquareNumbers([55, 43, 21]));
+
+
+
+
+// function reverseSentence(str: string): string {
+//     let words: string[] = str.split(" ");
+//     let result: string[] = [];
+//
+//     for (let i = 0; i < words.length; i++) {
+//         result.push(words[i].split("").reverse().join(""));
+//     }
+//
+//     return result.join(" ");
+// }
+//
+// console.log(reverseSentence("my name is ethan"));
