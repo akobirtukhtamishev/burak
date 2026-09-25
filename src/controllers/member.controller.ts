4e34fc2fe2125e01398318b349b138e9318b2852
akobirtukhtamishev <akobirtukhtamishev@gmail.com>
2026-09-25 16:10:50 +0900

    fix: modify reouting system and controllers

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -3,28 +3,4 @@ import {T} from "../libs/types/common"
 
 const memberController:T = {};
 
-memberController.goHome = (req: Request, res: Response) => {
-    try {
-        res.send("Home Page");
-    } catch (err) {
-        console.error("Error, goHome", err);
-    }
-};
-
-memberController.getLogin = (req: Request, res: Response) => {
-    try {
-        res.send("Login Page");
-    } catch (err) {
-        console.error("Error, getLogin", err);
-    }
-};
-
-memberController.getSignup = (req: Request, res: Response) => {
-    try {
-        res.send("Signup Page");
-    } catch (err) {
-        console.error("Error, goSignup", err);
-    }
-};
-
 export default memberController;
