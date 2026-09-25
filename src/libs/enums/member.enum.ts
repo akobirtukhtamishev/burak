bcb77dceab164828b8c84757fce46248d15d8876
akobirtukhtamishev <akobirtukhtamishev@gmail.com>
2026-09-25 17:06:45 +0900

    feat: develop member schema model

---
+++ src/libs/enums/member.enum.ts
@@ -0,0 +1,12 @@
+export enum MemberType {
+    USER = "USER",
+    RESTAURANT = "RESTAURANT",
+}
+
+export enum MemberStatus {
+    ACTIVE = "ACTIVE",
+    BLOCK = "BLOCK",
+    DELETE = "DELETE",
+}
+
+// enum qiymatlar bu aniq belgilangan qiymatlarnigina qabul qilish maqsadida ishlatiladigan type
